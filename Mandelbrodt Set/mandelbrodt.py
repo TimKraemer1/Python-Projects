b7275a956716ddab6a22bcb8d553865fb1d8e6e5
Tim Kraemer <timkraemer@Tims-Air.lan>
2023-08-01 20:43:19 -1000

    tested a method to zoom into the set

--- Mandelbrodt Set/mandelbrodt.py
+++ Mandelbrodt Set/mandelbrodt.py
@@ -10,22 +10,32 @@ def mandelbrot(c, max_iter):
     return n
 
 def draw_mandelbrot(x_min, x_max, y_min, y_max, width, height, max_iter):
-    x = np.linspace(x_min, x_max, width)
-    y = np.linspace(y_min, y_max, height)
-    mandelbrot_set = np.zeros((height, width))
+    while True:
+        x = np.linspace(x_min, x_max, width)
+        y = np.linspace(y_min, y_max, height)
+        mandelbrot_set = np.zeros((height, width))
 
-    for i in range(height):
-        for j in range(width):
-            c = x[j] + 1j * y[i]
-            mandelbrot_set[i, j] = mandelbrot(c, max_iter)
+        for i in range(height):
+            for j in range(width):
+                c = x[j] + 1j * y[i]
+                mandelbrot_set[i, j] = mandelbrot(c, max_iter)
 
+        plt.imshow(mandelbrot_set, extent=(x_min, x_max, y_min, y_max), cmap='hot', aspect='auto')
+        plt.title("Mandelbrot Set")
+        plt.xlabel("Real")
+        plt.ylabel("Imaginary")
+        plt.colorbar()
+        plt.draw()
+        plt.pause(0.1)  # Pause for a short time to allow for user input
 
-    plt.imshow(mandelbrot_set, extent=(x_min, x_max, y_min, y_max))
-    plt.title("Mandelbrot Set")
-    plt.xlabel("Real")
-    plt.ylabel("Imageinary")
-    plt.colorbar()
-    plt.show()
+        # Get new coordinates from user input to zoom into
+        print("Click on the two corners of the region you want to zoom into.")
+        points = plt.ginput(2, timeout=-1)
+
+        # Update the range with the new coordinates
+        x_min, y_min = points[0]
+        x_max, y_max = points[1]
+
 
 if __name__ == "__main__":
 
